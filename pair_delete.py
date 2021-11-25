447a7d0a2d77f5a59ca91ed258a97b69d5700a06
jongjin <jongjin.lee@stratiotechnology.com>
2021-11-25 09:17:20 +0900

    LJJ Clear

--- pair_delete.py
+++ pair_delete.py
@@ -1,6 +1,19 @@
-def mock(s):
-    one = [1,2,3,4,5]
-    two = [2,1,2,3,2,4,2,5]
-    three = [3,3,1,1,2,2,4,4,5,5]
-    return one
-print(mock)
+def solution(s):
+    stack = []
+    idx = 0
+    if len(s)%2 == 1:
+        return 0
+    while idx < len(s):
+        if not stack:
+            stack.append(s[idx])
+        elif stack[-1] == s[idx]:
+            stack.pop()
+        else:
+            stack.append(s[idx])
+        idx+=1
+        continue
+
+    if stack:
+        return 0
+    else:
+        return 1
